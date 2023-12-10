11ecdfd435315e97ac5ed9493d8601739d88c63a
Louis <louis.vanoise@epitech.eu>
2023-12-10 12:32:18 +0100

    add background image and transition x effect on name

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -3,9 +3,7 @@ import ProjectTable from "@/components/project-table";
 const Home = () => {
   return (
     <main className="w-full">
-      <div className="w-full px-8 bg-black h-[100vh]">
-        <ProjectTable />
-      </div>
+      <ProjectTable />
     </main>
   );
 };
--- components/project-table.tsx
+++ components/project-table.tsx
@@ -1,116 +1,160 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 const ProjectTable = () => {
+  const [backgroundImage, setBackgroundImage] = useState<string>("");
+
   const projects = [
     {
       name: "FRANCE.TV",
       about: "VISUAL IDENTITY",
       inquiries: "BROADCAST DESIGN",
       year: "2019",
+      image: "project-1",
     },
     {
       name: "EUROSPORT TOKYO 2020",
       about: "BRANDING",
       inquiries: "BROADCAST DESIGN",
       year: "2020",
+      image: "project-2",
     },
     {
       name: "JPOG",
       about: "BRANDING",
       inquiries: "FOOTBALL TEAM",
       year: "2022",
+      image: "project-3",
     },
     {
       name: "RUSH HOUR",
       about: "EXHIBITION",
       inquiries: "STREET PHOTOGRAPHY",
       year: "2021",
+      image: "project-1",
     },
     {
       name: "BENIN.TV",
       about: "VISUAL IDENTITY",
       inquiries: "BROADCAST DESIGN",
       year: "2018",
+      image: "project-2",
     },
     {
       name: "MISTER FREEZE NEXT LEVEL",
       about: "CREATIVE DIRECTION",
       inquiries: "STREET ART",
       year: "2016",
+      image: "project-3",
     },
     {
       name: "PARIS — ROUBAIX",
       about: "VISUAL IDENTITY",
       inquiries: "RACING",
       year: "2018",
+      image: "project-1",
     },
     {
       name: "ARTE HIP HOP",
       about: "ART DIRECTION",
       inquiries: "TV PROGRAM",
       year: "2022",
+      image: "project-2",
     },
     {
       name: "ARTE MULTIFACE",
       about: "ART DIRECTION",
       inquiries: "TV SHOW 2023",
       year: "2023",
+      image: "project-3",
     },
     {
       name: "PEACOCK SOCIETY",
       about: "BRANDING",
       inquiries: "FESTIVAL",
       year: "2020",
+      image: "project-1",
     },
     {
       name: "FOR ETERNITY",
       about: "VISUAL IDENTITY",
       inquiries: "METAVERSE",
       year: "2019",
+      image: "project-2",
     },
     {
       name: "THALASSA",
       about: "BRANDING",
       inquiries: "TV SHOW",
       year: "2017",
+      image: "project-3",
     },
     {
       name: "MANIFESTO",
       about: "VISUAL IDENTITY",
       inquiries: "FASHION STORE",
       year: "2015",
+      image: "project-1",
     },
     {
       name: "DICE",
       about: "VISUAL IDENTITY",
       inquiries: "RESTAURANT",
       year: "2016",
+      image: "project-2",
     },
   ];
+
+  const handleHover = (image: string) => {
+    if (image !== "") {
+      setBackgroundImage(`url('/assets/projects/${image}.jpg')`);
+    } else {
+      setBackgroundImage("");
+    }
+  };
   return (
-    <table className=" w-full h-[50%] table-auto text-sm text-white ">
-      <thead>
-        <tr className="">
-          <th className="text-left font-normal">GREGORY TEMPLE TATE</th>
-          <th className="text-left font-normal">ABOUT</th>
-          <th className="text-left font-normal">INQUIRIES</th>
-          <th className="text-left font-normal"></th>
-        </tr>
-      </thead>
-      <tbody className="">
-        {projects.map((project, index) => (
-          <tr
-            key={index}
-            className="border-b-[0.5px] border-color-white pb-10 mb-10"
-          >
-            <td>{project.name}</td>
-            <td>{project.about}</td>
-            <td>{project.inquiries}</td>
-            <td>{project.year}</td>
-          </tr>
-        ))}
-      </tbody>
-    </table>
+    <div
+      className="w-full px-8 bg-black h-[100vh] relative bg-cover transition duration-500 ease-in-out"
+      style={backgroundImage !== "" ? { backgroundImage: backgroundImage } : {}}
+    >
+      <div className="sticky top-0  z-50">
+        <table className="w-full h-[50%] table-auto  text-white text-xs">
+          <thead>
+            <tr>
+              <th className="text-left font-normal w-[30%]">
+                GREGORY TEMPLE TATE
+              </th>
+              <th className="text-left font-normal w-[25%]">ABOUT</th>
+              <th className="text-left font-normal w-[25%]">INQUIRIES</th>
+              <th className="text-left font-normal w-[20%]"></th>
+            </tr>
+          </thead>
+        </table>
+      </div>
+      <div className="overflow-auto h-[50%]">
+        <table className="w-full h-full table-auto text-xs text-white">
+          <tbody>
+            {projects.map((project, index) => (
+              <tr
+                key={index}
+                onMouseEnter={() => handleHover(project.image)}
+                onMouseLeave={() => handleHover("")}
+                className="border-b-[0.5px] border-color-white cursor-pointer py-10 group"
+                data-background={project.image}
+              >
+                <td className="w-[30%] transition duration-300 group-hover:translate-x-5">
+                  {project.name}
+                </td>
+                <td className="w-[25%]">{project.about}</td>
+                <td className="w-[25%]">{project.inquiries}</td>
+                <td className="w-[20%] text-right">{project.year}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
   );
 };
 
